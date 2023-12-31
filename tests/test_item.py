2976a9a720da50659dcc59d49345f64837c2b345
Rita <kapustnikova.rita@bk.ru>
2023-12-31 16:08:47 +0300

    добавлены тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+def test_init():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+    assert Item.all == [item1, item2]
+
+@pytest.fixture
+def my_object():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(my_object):
+    assert my_object.calculate_total_price() == 200000
+
+
+def test_apply_discount(my_object):
+    my_object.pay_rate = 1.5
+    my_object.apply_discount()
+    assert my_object.price == 15000
