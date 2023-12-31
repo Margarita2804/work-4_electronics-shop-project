d7382c314ba7cd8ffcb536102cbc106d2676ead0
Rita <kapustnikova.rita@bk.ru>
2023-12-31 16:13:14 +0300

    добавлены докстринги к тестам

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,7 +2,9 @@ import pytest
 
 from src.item import Item
 
+
 def test_init():
+    """Тестирование инициализации"""
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
     assert item1.name == "Смартфон"
@@ -10,16 +12,20 @@ def test_init():
     assert item1.quantity == 20
     assert Item.all == [item1, item2]
 
+
 @pytest.fixture
 def my_object():
+    """Фикстура возвращает экземпляр"""
     return Item("Смартфон", 10000, 20)
 
 
 def test_calculate_total_price(my_object):
+    """Тестирование общей стоимости товара"""
     assert my_object.calculate_total_price() == 200000
 
 
 def test_apply_discount(my_object):
+    """Тестирование применения скидки"""
     my_object.pay_rate = 1.5
     my_object.apply_discount()
     assert my_object.price == 15000
