2678f6b6982309683197142104545ac3ba629d74
Rita <kapustnikova.rita@bk.ru>
2024-01-31 22:57:05 +0300

    Добавлены пути

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,9 +2,9 @@ from src.item import Item
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv("test")
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv("src/crazy.csv")
     # InstantiateCSVError: Файл item.csv поврежден
